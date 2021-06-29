dd4455900084e6e9849e1534c193661a797455ac
GoDoyeon <97godo@naver.com>
2021-06-30 00:41:02 +0900

    ch09 컴포넌트 스타일링
    210630 ch09 컴포넌트 스타일링

---
+++ ch09/styling-react/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import StyledComponent from './StyledComponent';
+// import SassComponent from './SassComponent';
+// import CSSModule from './CSSModule';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {/* <SassComponent /> */}
+        {/* <CSSModule /> */}
+        <StyledComponent />
+      </div>
+    );
+  }
+}
+
+export default App;
